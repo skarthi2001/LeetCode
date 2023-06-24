35e623a967a3144f6f6388e736b9eca7854b33f7
skarthi2001 <karthik.s.2k01@gmail.com>
2023-06-24 22:25:52 +0530

    Added Programs

---
+++ Java/LongestSubstringWithoutRepeatChars.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class LongestSubstringWithoutRepeatChars {
+    public int lengthOfLongestSubstring(String str) {
+        int n = str.length();
+        int res = 0;
+        ArrayList <Character> visited = new ArrayList<Character>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (visited.contains((str.charAt(j)))) {
+                    visited.clear();
+                    break;
+                } else {
+                    visited.add((str.charAt(j)));
+                    if (res < j - i + 1) {
+                        res = j - i + 1;
+                    }
+
+                }
+            }
+
+        }
+        return res;
+    }
+}
---
+++ Java/ReverseInteger.java
@@ -0,0 +1,18 @@
+public class ReverseInteger {
+    public int reverse(int x) {
+        long rev = 0;
+        
+        while(x!=0)
+        {
+            rev = rev*10+(x%10);
+            x/=10;
+        }
+        
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) 
+        {
+            return 0;
+        }
+        
+        return (int)rev;
+    }
+}
